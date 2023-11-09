5fbe3fee87f3748137716a68e4e3aed0998516b1
Jared Ottley <jared.ottley@hyland.com>
2023-11-09 11:56:29 -0700

    Clean and Update Dependencies

---
+++ _ci/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Build&Test Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+mvn -B -U \
+    clean install
+
+popd
+set +vex
+echo "=========================== Finishing Build&Test Script =========================="
---
+++ _ci/release.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Release Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+# For PR builds only execute a Dry Run of the release
+[ "${PULL_REQUEST}" = "false" ] && DRY_RUN="" || DRY_RUN="-DdryRun"
+
+# Travis CI runner work on DETACHED HEAD, so we need to checkout the release branch
+git checkout -B "$BRANCH_NAME"
+
+#sync the branch by getting the latest changes
+git pull
+
+git config user.email "${GIT_EMAIL}"
+git config user.name "${GIT_USERNAME}"
+
+# Run the release plugin - with "[skip ci]" in the release commit message
+mvn -B \
+  ${DRY_RUN} \
+  "-Darguments=-DskipTests -Dmaven.javadoc.skip -Dadditionalparam=-Xdoclint:none" \
+  release:clean release:prepare release:perform \
+  -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
+  -Dusername="${GIT_USERNAME}" \
+  -Dpassword=${GIT_PASSWORD}
+
+popd
+set +vex
+echo "=========================== Finishing Release Script =========================="
